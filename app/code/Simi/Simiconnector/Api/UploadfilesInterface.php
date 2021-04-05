10d0b46f0e64e1eea2d2a5e9e06a9119ad32b09a
hung <quochung@simicart.com>
2021-04-05 16:21:12 +0700

    update upload file interface

---
+++ app/code/Simi/Simiconnector/Api/UploadfilesInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Simi\Simiconnector\Api;
+
+interface UploadfilesInterface
+{
+    /**
+     * @return array
+     */
+    public function store();
+}
