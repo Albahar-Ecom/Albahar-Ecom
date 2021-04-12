18f3557de40e8917b1b023ec0ac82284f05f26e7
hung <quochung@simicart.com>
2021-04-12 15:30:13 +0700

    add config for footer

--- app/code/Simi/Simicustomize/Observer/GetStoreviewInfoAfter.php
+++ app/code/Simi/Simicustomize/Observer/GetStoreviewInfoAfter.php
@@ -49,4 +49,53 @@ class GetStoreviewInfoAfter implements ObserverInterface
                 $this->storeManager->getStore()->getCode());
     }
 
+    public function getSerializedConfigValue( $value ) {
+		if ( empty( $value ) ) {
+			return false;
+		}
+		if ( $this->isSerialized( $value ) ) {
+			$unserializer = $this->simiObjectManager->get( \Magento\Framework\Unserialize\Unserialize::class );
+		} else {
+			$unserializer = $this->simiObjectManager->get( \Magento\Framework\Serialize\Serializer\Json::class );
+		}
+
+		return $unserializer->unserialize( $value );
+	}
+
+
+    public function getFooterConfig() {
+		$facebookLink  = $this->getStoreConfig( 'siminiaconfig/footer/facebook_link' );
+		$instagramLink = $this->getStoreConfig( 'siminiaconfig/footer/instagram_link' );
+		$youtubeLink   = $this->getStoreConfig( 'siminiaconfig/footer/youtube_link' );
+        $menuTitle1    = $this->getStoreConfig( 'siminiaconfig/footer/footer_1_title' );
+		$menusListing1 = $this->getStoreConfig( 'siminiaconfig/footer/footer_1_menu' );
+        $menuTitle2    = $this->getStoreConfig( 'siminiaconfig/footer/footer_2_title' );
+		$menusListing2 = $this->getStoreConfig( 'siminiaconfig/footer/footer_2_menu' );
+		
+        $menus1        = [];
+		$list         = $this->getSerializedConfigValue( $menusListing1 );
+		if ( $list && count( $list ) > 0 ) {
+			foreach ( $list as $key => $item ) {
+				$menus1[] = $item;
+			}
+		}
+
+        $menus2       = [];
+		$list         = $this->getSerializedConfigValue( $menusListing2 );
+		if ( $list && count( $list ) > 0 ) {
+			foreach ( $list as $key => $item ) {
+				$menus2[] = $item;
+			}
+		}
+
+		return [
+			'facebook_link'  => $facebookLink,
+			'instagram_link' => $instagramLink,
+			'youtube_link'   => $youtubeLink,
+            'menu_title_1'   => $menuTitle1,
+            'menu_title_2'   => $menuTitle2,
+			'menus_1'        => $menus1,
+            'menus_2'        => $menus2,
+		];
+	}
 }
